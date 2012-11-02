a6031d12cfb967a9210c1f6088bccc9442f25943
tokuhirom <tokuhirom@gmail.com>
2012-11-02 14:59:18 +0900

    implemented

--- lib/Test/Stub.pm
+++ lib/Test/Stub.pm
@@ -1,10 +1,41 @@
 package Test::Stub;
 use strict;
 use warnings;
-use 5.010001;
+use 5.008001;
 our $VERSION = '0.01';
+use parent qw/Exporter/;
 
+our @EXPORT = qw/stub/;
 
+sub stub {
+    bless \$_[0], 'Test::Stub::Driver';
+}
+
+package Test::Stub::Driver;
+
+my $id = 0;
+our $AUTOLOAD;
+sub DESTROY { }
+sub AUTOLOAD {
+    my $self = shift;
+
+    $AUTOLOAD =~ s/.*:://;
+    my $method = $AUTOLOAD;
+    my $stub = shift || sub {};
+    my $func = ref($stub) eq 'CODE' ? $stub : sub { $stub };
+
+    my $pkg = "Test::Stub::Anon" . $id++;
+
+    # stubbing.
+    {
+        no strict 'refs';
+        unshift @{"$pkg\::ISA"}, ref $$self;
+        *{"$pkg\::$method"} = $func;
+    }
+
+    # rebless
+    bless $$self, $pkg;
+}
 
 1;
 __END__
@@ -13,15 +44,41 @@ __END__
 
 =head1 NAME
 
-Test::Stub - ...
+Test::Stub - Stub! Stub! Stub!
 
 =head1 SYNOPSIS
 
-  use Test::Stub;
+    use Test::Stub;
+
+    my $agent = LWP::UserAgent->new();
+    stub($agent)->get(HTTP::Response->new(200, "OK"));
+    is($agent->get('http://www.aiseikai.or.jp/')->code, 200);
 
 =head1 DESCRIPTION
 
-Test::Stub is
+Test::Stub is a simple stubbing library for Perl5.
+
+=head1 EXPORTABLE FUNCTIONS
+
+=over 4
+
+=item stub($stuff) : Test::Stub::Driver
+
+Create a new instance of Test::Stub::Driver.
+
+=back
+
+=head1 Test::Stub::Driver
+
+This class only provides a AUTOLOAD method.
+
+AUTOLOAD method rebless the C<$stuff> to anonymous class.
+
+For example. After calling following code:
+
+    stub($stuff)->foo('bar');
+
+C<$stuff->foo()> returns 'bar'.
 
 =head1 AUTHOR
 
--- xt/01_podspell.t
+++ xt/01_podspell.t
@@ -58,3 +58,5 @@ Gosuke
 Miyashita
 mysqldiff
 mmm
+rebless
+EXPORTABLE
