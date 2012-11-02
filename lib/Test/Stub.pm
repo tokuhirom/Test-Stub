b543ea9cd2c52dd331fd38bd1690bb516a6f9dbf
tokuhirom <tokuhirom@gmail.com>
2012-11-02 14:39:11 +0900

    initial import

---
+++ lib/Test/Stub.pm
@@ -0,0 +1,39 @@
+package Test::Stub;
+use strict;
+use warnings;
+use 5.010001;
+our $VERSION = '0.01';
+
+
+
+1;
+__END__
+
+=encoding utf8
+
+=head1 NAME
+
+Test::Stub - ...
+
+=head1 SYNOPSIS
+
+  use Test::Stub;
+
+=head1 DESCRIPTION
+
+Test::Stub is
+
+=head1 AUTHOR
+
+Tokuhiro Matsuno E<lt>tokuhirom AAJKLFJEF@ GMAIL COME<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+Copyright (C) Tokuhiro Matsuno
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
